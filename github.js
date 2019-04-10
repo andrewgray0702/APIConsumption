d00c4a9bba60ba95cd3d6c915f47d1025bd27e9e
Grayton Savickas <graytonsavickas@graytons-air.mlc.edu>
2019-04-10 14:47:06 -0500

    names need updating in html

--- github.js
+++ github.js
@@ -1,14 +1,14 @@
-var results = "";
+var results = "";//  <--- results or .results IS TO BE RENAMED according to new css / html values
         // Below is a promise chain .then the return of .then is sent to the next .then
         document.getElementById('submit').addEventListener("click", function(e){
             e.preventDefault();
             document.querySelectorAll('figure').forEach(val => val.remove());
             document.querySelector(".results").innerText = "";
-            var jobLocal = document.querySelector("#jobLoCal").value;
+            var jobLocal = document.querySelector("#loc").value;
             console.log(jobLocal);
-            var jobDescription = document.querySelector("#jobDescription").value;;
+            var jobDescription = document.querySelector("#desc").value;;
             console.log(jobDescription);
-            var jobType = document.querySelector("#type").value;
+            var jobType = document.querySelector("#full").value;
             console.log(jobType);
             if(jobLocal.length != 0) {
             
@@ -17,15 +17,14 @@ var results = "";
             .then(function(json){
               document.querySelector(".results").innerText = "Search has returned " + json.totalResults + " results"; // This is where to send the information we get from the API
               json.Search.forEach(function(val){
-                var newFigure = document.createElement("figure");
-                var newFigcaption = document.createElement("figcaption");
-                var newImg = document.createElement("img");
-                var companyName = document.createElement("h2");
-                newFigure.appendChild(newImg);
-                newImg.src = val.Poster;
-                newFigure.appendChild(newFigcaption);
-                newFigcaption.innerText = val.Title + " Year: " + val.Year ;
-                document.querySelector('#containerResults').appendChild(newFigure);
+                var jobPost = document.createElement("div");
+                var companyName = document.createElement("h3");
+                var companyUrl = document.createElement("a");
+                companyUrl.appendChild(companyUrl);
+                companyUrl.href = val.company_url;
+                companyName.innerText = val.company;
+                jobPost.innerText = val.title + " Location: " + val.location ;
+                document.querySelector('#containerResults').appendChild(newFigure); // <--- containerResults to be RENAMED
               })
             })
         }
