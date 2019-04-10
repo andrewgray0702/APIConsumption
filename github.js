5fdd88b0adb68d6dee9e916c11bcbac0e180d29e
Boxor <cdschonlau@hotmail.com>
2019-04-10 15:17:11 -0500

    Adjusted to match this project

--- github.js
+++ github.js
@@ -1,12 +1,11 @@
-var results = "";//  <--- results or .results IS TO BE RENAMED according to new css / html values
+
         // Below is a promise chain .then the return of .then is sent to the next .then
         document.getElementById('submit').addEventListener("click", function(e){
             e.preventDefault();
             document.querySelectorAll('figure').forEach(val => val.remove());
-            document.querySelector(".results").innerText = "";
             var jobLocal = document.querySelector("#loc").value;
             console.log(jobLocal);
-            var jobDescription = document.querySelector("#desc").value;;
+            var jobDescription = document.querySelector("#desc").value;
             console.log(jobDescription);
             var jobType = document.querySelector("#full").value;
             console.log(jobType);
@@ -15,7 +14,6 @@ var results = "";//  <--- results or .results IS TO BE RENAMED according to new 
             fetch('https://jobs.github.com/positions.json?description+=' + jobDescription + '&location=' + jobLocal + '&type=' + jobType)
             .then(response => response.json())
             .then(function(json){
-              document.querySelector(".results").innerText = "Search has returned " + json.totalResults + " results"; // This is where to send the information we get from the API
               json.Search.forEach(function(val){
                 var jobPost = document.createElement("div");
                 var companyName = document.createElement("h3");
@@ -24,12 +22,12 @@ var results = "";//  <--- results or .results IS TO BE RENAMED according to new 
                 companyUrl.href = val.company_url;
                 companyName.innerText = val.company;
                 jobPost.innerText = val.title + " Location: " + val.location ;
-                document.querySelector('#containerResults').appendChild(newFigure); // <--- containerResults to be RENAMED
-              })
-            })
+                document.querySelector('#resultsArea').appendChild(jobPost); // <--- #resultsArea has been RENAMED
+              });
+            });
         }
             else {
-                document.querySelector('#Error').innerText = "Location is Required"
+                document.querySelector('#Error').innerText = "Location is Required";
             }
         });
         function buildEntry(){
