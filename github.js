015be50ca16f6ef1ac39f02ce44d59d42050312f
Grayton Savickas <graytonsavickas@Graytons-MacBook-Air.local>
2019-04-10 19:34:19 -0500

    changes to the API

--- github.js
+++ github.js
@@ -1,5 +1,3 @@
-
-// Below is a promise chain .then the return of .then is sent to the next .then
 document.querySelector('#submitButton').addEventListener("click", function (e) {
   e.preventDefault();
   document.querySelectorAll('.jobPost').forEach(val => val.remove());
@@ -10,32 +8,27 @@ document.querySelector('#submitButton').addEventListener("click", function (e) {
   var jobType = document.querySelector("#full").checked;
   console.log(jobType);
   if (jobDescription.length != 0) {
-    // https://jobs.github.com/positions.json?page=1&Search&location=new york
-    // var queryString = 'https://us.jooble.org/api/&description=' + jobDescription + '&location=' + jobLocal + '&type=' + jobType "61d82599-8f71-4526-ae47-6af3d399ee0b"
-    var queryString = 'https://jobs.search.gov/jobs/search.json?query=' + jobDescription;
-    // var request = new Request(queryString, {method: 'GET', mode: 'no-cors'})
-    fetch(queryString).then(response => response.json())
-      .then(function (json) {
-        console.log("here");
-        json.forEach(function (val) {
-          var jobPost = document.createElement("div"); jobPost.className = "jobPost";
-          var companyName = document.createElement("h3");
-          var companyUrl = document.createElement("a");
-          var description = document.createElement("div");
-          var localPosition = document.createElement("div");
-          description.innerText = val.position_title;
-          localPosition.innerText = "Position: " + val.position_title+ " Location: " + val.locations;
-          companyUrl.href = val.company_url;
-          companyName.innerText = val.organization_name;
-          jobPost.appendChild(companyName);
-          jobPost.appendChild(companyUrl);
-          jobPost.appendChild(description);
-          jobPost.appendChild(localPosition);
-          document.querySelector('#resultsArea').appendChild(jobPost); // <--- containerResults to be RENAMED
-        })
-      })
+  var queryString = 'https://jobs.search.gov/jobs/search.json?query=' + jobDescription;
+  fetch(queryString).then(response => response.json())
+  .then(function (json) {
+  console.log("here");
+  json.forEach(function (val) {
+  var jobPost = document.createElement("div"); jobPost.className = "jobPost";
+  var companyName = document.createElement("h3");
+  var companyUrl = document.createElement("a");
+  var description = document.createElement("div");
+  var localPosition = document.createElement("div");
+  description.innerText = val.position_title;
+  localPosition.innerText = "Position: " + val.position_title+ " Location: " + val.locations;
+  companyUrl.href = val.company_url;
+  companyName.innerText = val.organization_name;
+  jobPost.appendChild(companyName);
+  jobPost.appendChild(companyUrl);
+  jobPost.appendChild(description);
+  jobPost.appendChild(localPosition);
+  document.querySelector('#resultsArea').appendChild(jobPost); 
+  })
+  })
   }
-});
-function buildEntry() {
-
-}
+  });
+ 
